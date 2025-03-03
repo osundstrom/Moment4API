a08f9ab643ae06f013eb63c51580aa0d143aa620
osundstrom <oskar.sundstrom123@gmail.com>
2025-03-03 21:52:54 +0100

    intendering och kommentarer

--- Program.cs
+++ Program.cs
@@ -3,61 +3,71 @@ using Microsoft.EntityFrameworkCore;
 var builder = WebApplication.CreateBuilder(args);
 
 //Dbcontext och sqlite som databas
-builder.Services.AddDbContext<SongDb>(options => 
+builder.Services.AddDbContext<SongDb>(options =>
     options.UseSqlite("Data Source = SongDb.db")
 );
 
 var app = builder.Build();
 
+//--------------------------------GET-----------------------------------------//
 //Routes, standard vid bara /
-app.MapGet("/" , ()  => "Välkommen till musik API!");
+app.MapGet("/", () => "Välkommen till musik API!");
 
 //Get vid /songs
-app.MapGet("/songs" , async(SongDb songDb) => {
+app.MapGet("/songs", async (SongDb songDb) =>
+{
     var songs = await songDb.Songs.ToListAsync(); //sätter songs till alla låtar
     return Results.Ok(songs); //returnerar all låtar
 });
 
 //Get specifik låt med id vid songs/id
-app.MapGet("/songs/{id}", async(int id, SongDb songDb) => {
-   var thisSong = await songDb.Songs.FindAsync(id); //hittar baserat på id
+app.MapGet("/songs/{id}", async (int id, SongDb songDb) =>
+{
+    var thisSong = await songDb.Songs.FindAsync(id); //hittar baserat på id
 
-     if (thisSong != null) {
+    if (thisSong != null){
         return Results.Ok(thisSong); //om låten finns 
-    }else {
+    }else{
         return Results.NotFound("ID finns ej"); //om id ej finns
     };
 
 });
 
+//--------------------------------POST-----------------------------------------//
 
 //Post vid /songs
-app.MapPost("/songs", async(Song song, SongDb songDb) => {
-
+app.MapPost("/songs", async (Song song, SongDb songDb) =>
+{
     songDb.Songs.Add(song); //läggert ill i db
     await songDb.SaveChangesAsync(); //sparar
 
     return Results.Created("Tillagd:", song); //returnerar
 });
 
+//--------------------------------DELETE-----------------------------------------//
+
 //Delete via id
-app.MapDelete("/songs/{id}", async(int id, SongDb songDb) => {
+app.MapDelete("/songs/{id}", async (int id, SongDb songDb) =>
+{
     var thisSong = await songDb.Songs.FindAsync(id); //hittar baserat på id
-    
-    if (thisSong != null) { 
+
+    if (thisSong != null){
         songDb.Songs.Remove(thisSong); //tar bort om låt finns
         await songDb.SaveChangesAsync(); //sparar
         return Results.Ok("Raderad"); //returnerar 
-    }else {
-       return Results.NotFound("ID finns ej"); //om ej hittas
+    }else{
+        return Results.NotFound("ID finns ej"); //om ej hittas
     }
 });
 
+//--------------------------------PUT-----------------------------------------//
+
 //Put
-app.MapPut("/songs/{id}", async(int id, Song newSong, SongDb songDb) => {
+app.MapPut("/songs/{id}", async (int id, Song newSong, SongDb songDb) =>
+{
     var thisSong = await songDb.Songs.FindAsync(id); //hittar baserat på id
 
-    if (thisSong != null) { //om låten skild fårn null, så byter vi gamla mot nya inmatade
+    if (thisSong != null){ //om låten skild fårn null, så byter vi gamla mot nya inmatade
         thisSong.artist = newSong.artist;
         thisSong.title = newSong.title;
         thisSong.length = newSong.length;
@@ -65,32 +75,39 @@ app.MapPut("/songs/{id}", async(int id, Song newSong, SongDb songDb) => {
 
         await songDb.SaveChangesAsync();//sparar
         return Results.Ok(new { Message = "Uppdaterad:", Song = newSong }); //returnerar 
-    }else {
+    }else{
         return Results.NotFound("ID finns ej"); //om ej hittas
     }
 
 });
+
+//-------------------------------------------------------------------------//
 
 
 //starta
 app.Run();
 
 
+//--------------------------------Klass och dbcontext-----------------------------------------//
+
+
 //klass för låt, med id, namn, artist, längd, genre.
-public class Song {
-    public int id  {get; set; }
-    public string? title  {get; set; }
-    public string? artist  {get; set; }
-    public int length  {get; set; }
-    public string? category  {get; set; }
+public class Song
+{
+    public int id { get; set; }
+    public string? title { get; set; }
+    public string? artist { get; set; }
+    public int length { get; set; }
+    public string? category { get; set; }
 }
 
 
 //DbContext
-public class SongDb : DbContext {
+public class SongDb : DbContext
+{
     public SongDb(DbContextOptions<SongDb> options)
     : base(options) { }
 
     //dbSet
-    public DbSet<Song> Songs => Set<Song>(); 
+    public DbSet<Song> Songs => Set<Song>();
 }
